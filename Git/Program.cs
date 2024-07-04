5df0214827f454588c2e112b098bd7c372527f1d
GazingStars <md.code@rambler.ua>
2024-07-04 18:58:32 +0300

    feat(Flat.cs): added fields

---
+++ Git/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+class Flat
+{
+    private uint number;
+    private byte citizens;
+    private byte floor;
+
+
+}
